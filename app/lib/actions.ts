8fef571fb849da31a9f5fc0d8a5449337cb48150
AshkanKeyhan <ashkankeyhan50@gmail.com>
2026-05-02 19:13:22 +0330

    Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { pool } from './pgpool';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -21,6 +23,25 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 export type State = {
   errors?: {
@@ -112,6 +133,7 @@ export async function deleteInvoice(id: string) {
   );
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+  
 
   
 }
